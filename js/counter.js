873bbfa68a2f0c0306db64b3bf1d3f4b9a2f726b
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2023-01-31 21:12:22 +0100

    counters

--- js/counter.js
+++ js/counter.js
@@ -1,49 +1,16 @@
-const refs = {
-    saleCounter: document.querySelector(".js-sale-counter"),
-    devCounter: document.querySelector(".js-dev-counter")
-}
+let values = document.querySelectorAll (".counters");
+let interval = 1000;
 
-let saleCount = 0
-let devCount = 0
+values.forEach((value) => {
+    let startValue = 0;
+    let endValue = parseInt(value.getAttribute("data-target"))
+    let duration = Math.floor(interval/endValue)
+    let counter = setInterval(function () {
+        startValue += 1;
+        value.textContent=startValue;
+        if (startValue == endValue) {
+            clearInterval(counter)
+        }
+    }, duration)
 
-function saleCounter () {
-        saleCount++
-    refs.saleCounter.textContent = saleCount;
-}
-
-function devCounter () {
-    devCount++
-refs.devCounter.textContent = devCount;
-}
-
-setInterval(function() {
-    if(saleCount < 22) {
-        saleCounter()
-    }
-}, 50)
-
-setInterval(function() {
-    if(devCount < 84) {
-        devCounter()
-    }
-}, 50)
-
-
-// let saleCount = 0
-// let devCount = 0
-
-// function counter () {
-//     if(saleCount < 22) {
-//         saleCount++
-//     }
-
-//     if(devCount < 84) {
-//         devCount++
-//     }
-//     refs.saleCounter.textContent = saleCount;
-//     refs.devCounter.textContent = devCount;
-// }
-
-// setInterval(function() {
-//     counter()
-// }, 50)
+})
