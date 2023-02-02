499c54fb05fac88c3509e375df93c7348515c8f1
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2023-02-02 16:42:05 +0100

    progress js

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ import "./js/counter"
 
 
 
-
-
 // import { setupCounter } from './counter.js'
 
 // document.querySelector('#app').innerHTML = `
@@ -30,3 +28,34 @@ import "./js/counter"
 // `
 
 // setupCounter(document.querySelector('#counter'))
+
+
+const statuses = document.querySelectorAll(".status")
+const actives = document.querySelectorAll (".progress-active");
+const progressLine = document.querySelector(".progress-line-active")
+
+function progress ( ){
+    let length = statuses.length
+
+    let width = 1200/length/2
+    console.log(width)
+    if (statuses[0].className.includes("active")){
+        progressLine.style.width = `${width}px`
+                //  progressLine.classList.add("length-1")
+             } else if (statuses[1].className.includes("active")){
+                // progressLine.classList.add("length-2")
+             } else if (statuses[2].className.includes("active")) {
+                // progressLine.classList.add("length-3")
+                progressLine.style.width="200px"
+             } 
+}
+
+progress()
+
+// const progressLineActive = document.querySelector(".progress-line-active")
+
+// function length () {
+//     if (dots.dot1.className.includes("finished")){
+//         progressLineActive.className = "w-1"
+//     }
+// }
